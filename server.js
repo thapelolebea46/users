ad97453fac5e117b14139944820079c022173a84
thapelolebea46 <thapelolebea46@gmail.com>
2025-04-16 23:27:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(` Thapelo Server running at${PORT}`);
 });
